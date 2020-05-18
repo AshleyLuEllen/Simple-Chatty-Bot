c4e7b7a11d52e9f42d8600d3abf94c09458d0f93
AshleyLuEllen <ashleylu_couch1@baylor.edu>
2020-05-18 11:55:29 -0700

    Stage 3 completed

--- bot.py
+++ bot.py
@@ -1,5 +1,13 @@
 print('Hello! My name is Aid.')
 print('I was created in 2020.')
 print('Please, remind me your name.')
+
 name = input()
-print('What a great name you have,', name + '!')
+
+print('What a great name you have, ' + name + '!')
+print('Let me guess your age.')
+print('Enter remainders of dividing your age by 3, 5 and 7.')
+
+# reading all remainders
+age = (int(input()) * 70 + int(input()) * 21 + int(input()) * 15) % 105
+print("Your age is " + str(age) + "; that's a good time to start programming!")
